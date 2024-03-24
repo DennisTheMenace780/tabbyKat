a2a3953ca163f133d9bbe58543c6f575cceac591
Dennis Gray <djgray780@gmail.com>
2024-03-23 22:03:42 -0700

    update

--- model.go
+++ model.go
@@ -67,7 +67,7 @@ func (m Model) View() string {
 	// 	return QuitCheckoutStyle.Render(m.err)
 	// }
 	if m.choice != "" {
-		return ""
+		return QuitCheckoutStyle.Render("checking out")
 	}
 	// if m.quitting {
 	// 	return QuitTextStyle.Render("Not hungry? That’s cool.")
